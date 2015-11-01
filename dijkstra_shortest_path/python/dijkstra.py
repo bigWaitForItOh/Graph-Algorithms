d4ead4ef1fb20f51496073bf906ae701a4b5d867
Raghav Dua <duaraghav8@gmail.com>
2015-11-01 17:31:55 +0530

    Update dijkstra.py

--- dijkstra_shortest_path/python/dijkstra.py
+++ dijkstra_shortest_path/python/dijkstra.py
@@ -1,9 +1,9 @@
 ###########################################################################################################################
-#IMPLEMENTATION OF Dijkstra's Shortest Path Algorithm for connected, weighted, directed graphs. Yields shortest path LENGTHS from
+#IMPLEMENTATION OF Dijkstra's Shortest Path Algorithm for weighted, directed graphs. Yeilds shortest path LENGTHS from
 #source to every other vertex (needs to be modified to include path information).
 ###########################################################################################################################
 
-#takes the graph (described below) and the source vertex as arguments and returns a dictionary which contains all the vertices and their corresponding shortest path lengths
+#takes the graph (described below) and the source vertex as arguments and returns a dictionary which contains all the vertices and their corresponding shortest path lengths. Returns -1 if the Vertex is unreachable.
 def dijkstra (graph, start):
 	spt_set, dists = set (), { node : 0 if node == start else float ('inf') for node in list (graph) };
 
@@ -14,6 +14,12 @@ def dijkstra (graph, start):
 				nearest = (node, dists [node]);
 		spt_set.add (nearest [0]);
 
+		try:
+			neighbours = graph [nearest [0]];
+		except Exception as e:
+			for node in dists: dists [node] = -1 if dists [node] == float ('inf') else dists [node];
+			return (dists);
+
 		for node in graph [nearest [0]]:
 			if (dists [node [0]] > (nearest [1] + node [1])):
 				dists [node [0]] = nearest [1] + node [1];
@@ -21,17 +27,19 @@ def dijkstra (graph, start):
 
 #EXAMPLE OF A GRAPH
 #REPRESENTED BY A DICTIONARY, WHERE A KEY REPRESENTS THE NODE FROM WHICH THE EDGE STARTS. A key's value is a set of Tuples. The first element of each tuple is the Node the edge reaches. The second element is the weight of the edge. The list contains 1 tuple for every neighbour the KEY has.
-#EXAMPLE: A is connected to B with weight 2 and C with weight 3.
+#EXAMPLE: A is connected to B with weight 2 and C with weight 3 and X & Y are unreachable Vertices
 graph = {
 	'A': set ([ ('B', 2), ('C', 3) ]),
 	'B': set ([ ('C', 3), ('D', 5), ('E', 8) ]),
 	'C': set ([ ('D', 6) ]),
 	'D': set ([ ('E', 8), ('F',6) ]),
 	'E': set ([ ('F', 6) ]),
-	'F': set ([])
+	'F': set ([]),
+	'X': set ([]),
+	'Y': set ([])
 };
 
 #SAMPLE CALL TO THE FUNCTION
 dists = dijkstra (graph, 'A');
 print ('Distances from A to all nodes are: ');
-for i in x: print (i, x [i]);
+for i in dists: print (i, dists [i]);
